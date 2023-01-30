cb6c872332ca1ff6f90f296f89843bc37854d5e6
yochi66865 <yochi66865@gmail.com>
2023-01-30 20:45:26 +0200

    fix IntersectionObserver

--- src/app/tabs/tabs.component.ts
+++ src/app/tabs/tabs.component.ts
@@ -9,6 +9,7 @@ import {
   ViewChildren,
   ViewEncapsulation,
 } from '@angular/core';
+import { maxBy } from 'lodash';
 
 @Component({
   selector: 'app-tabs',
@@ -22,29 +23,52 @@ export class TabsComponent implements AfterViewInit, OnChanges {
   @ViewChildren('section')
   sectionElements!: QueryList<ElementRef>;
   selectedSectionId: number = 0;
-  observer: IntersectionObserver = new IntersectionObserver(
-    (entries) => {
-      if (entries[0].isIntersecting === true) {
-        this.selectedSectionId = +entries[0].target.id;
-      }
-    },
-    { threshold: [0] }
-  );
+  observer!: IntersectionObserver;
+  itemsOnScreen2: Set<IntersectionObserverEntry> = new Set();
+  itemsOnScreen: Record<string, IntersectionObserverEntry> = {};
 
   constructor() {}
   ngOnChanges(changes: SimpleChanges): void {}
 
   ngAfterViewInit(): void {
+    this.observer = new IntersectionObserver(
+      (entries) => {
+        this.mapSectionsOnScreen(entries);
+        this.selectedSectionIdByMaxIntersectionRatio();
+      },
+      {
+        threshold: [0, 0.25, 0.75, 1],
+        root: this.sidenavContent.nativeElement,
+      }
+    );
     console.log('sectionElements', this.sectionElements);
     this.sectionElements.forEach((section) =>
       this.observer.observe(section.nativeElement)
     );
   }
+
+  mapSectionsOnScreen(sections: IntersectionObserverEntry[]) {
+    sections.forEach((section) => {
+      if (section.isIntersecting) {
+        this.itemsOnScreen[section.target.id] = section;
+      } else {
+        delete this.itemsOnScreen[section.target.id];
+      }
+    });
+  }
+
+  selectedSectionIdByMaxIntersectionRatio() {
+    this.selectedSectionId = +(
+      maxBy(Object.values(this.itemsOnScreen), 'intersectionRatio')?.target
+        ?.id ?? 0
+    );
+  }
+
   scrollTo(sectionIndex: number) {
     let sectionOffsetTop =
       this.sectionElements.get(sectionIndex)?.nativeElement?.offsetTop ?? 0;
     (this.sidenavContent.nativeElement as HTMLElement).scrollTo({
-      top: sectionOffsetTop,
+      top: sectionOffsetTop + 1,
     });
   }
 
